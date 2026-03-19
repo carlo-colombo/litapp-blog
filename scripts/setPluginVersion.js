aec848322581fe06c778273736c107a471064c70
Carlo Colombo <carlo.colombo@gmx.com>
2026-03-19 08:40:11 +0100

    chore: update GitHub Actions and dependencies

--- scripts/setPluginVersion.js
+++ scripts/setPluginVersion.js
@@ -2,15 +2,18 @@ import fs from "fs";
 
 const path = "./plugins/litapp/blog/plugin.info";
 
-const version = JSON.parse(fs.readFileSync("./package.json")).version;
-const pluginInfo = JSON.parse(fs.readFileSync(path));
+const versionFromArgs = process.argv[2];
+const versionFromPackage = JSON.parse(fs.readFileSync("./package.json")).version;
 
-console.log(version);
+const version = (versionFromArgs || versionFromPackage).replace(/^v/, "");
+
+console.log(`Setting plugin version to: ${version}`);
 
 if (!version) {
   process.exit(1);
 }
 
+const pluginInfo = JSON.parse(fs.readFileSync(path));
 pluginInfo.version = version;
 
-fs.writeFileSync(path, JSON.stringify(pluginInfo, " ", 4));
+fs.writeFileSync(path, JSON.stringify(pluginInfo, null, 4) + "\n");
